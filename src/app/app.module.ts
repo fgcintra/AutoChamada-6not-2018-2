c78f90275e7009a677252796aaa23e0177c1c2c7
fgcintra <fausto@fgcintra.pro.br>
2018-09-06 23:57:39 -0300

    Restarting...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { AngularFireModule } from 'angularfire2';
 export const firebaseConfig = environment.firebaseConfig;
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
+
 @NgModule({
   declarations: [
     MyApp,
